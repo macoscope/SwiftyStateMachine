4cb9fa828e47334c9fe9c5e55f216f690644d299
Maciej Konieczny <narf@macoscope.net>
2015-03-24 10:45:33 +0100

    LABSM-6: added empty iOS framework

---
+++ StateMachine/StateMachine.h
@@ -0,0 +1,19 @@
+//
+//  StateMachine.h
+//  StateMachine
+//
+//  Created by Maciej Konieczny on 2015-03-24.
+//  Copyright (c) 2015 Macoscope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StateMachine.
+FOUNDATION_EXPORT double StateMachineVersionNumber;
+
+//! Project version string for StateMachine.
+FOUNDATION_EXPORT const unsigned char StateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateMachine/PublicHeader.h>
+
+

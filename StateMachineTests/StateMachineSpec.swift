a2acfb354a8d2690443ff3c44e513ed7529af0f7
Maciej Konieczny <narf@macoscope.net>
2015-03-24 12:02:39 +0100

    LABSM-7: added Quick spec with a failing test

---
+++ StateMachineTests/StateMachineSpec.swift
@@ -0,0 +1,23 @@
+//
+//  StateMachineSpec.swift
+//  StateMachineSpec
+//
+//  Created by Maciej Konieczny on 2015-03-24.
+//  Copyright (c) 2015 Macoscope. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+
+class StateMachineSpec: QuickSpec {
+    override func spec() {
+        describe("State Machine") {
+
+            it("has a failing test") {
+                expect("foo") == "bar"
+            }
+
+        }
+    }
+}
